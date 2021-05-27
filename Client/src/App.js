b1e100885169e6a16e32f6a838ee9a3b683b4145
felipevillajr <felipevilla2010@live.com>
2021-05-27 15:21:50 -0400

    added main Routes to app.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import Signin from "./Pages/SignIn/Signin";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signin" component={Signin} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ Client/src/Pages/Profile/Profile.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Profile.scss";
+
+export default function Profile() {
+  return <div></div>;
+}
